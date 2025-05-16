0d902ac93aeb453e43542095bb6c69147fb3e4e0
wjdehddn <wjdehddn30@gmail.com>
2025-05-16 20:40:27 +0900

    Update axios.ts

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: '/',
-  withCredentials: true,
+  withCredentials: false,
 });
 
 instance.interceptors.request.use((config) => {
